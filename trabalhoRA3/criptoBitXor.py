0f86c8782ab2e5227f27d222a3a2ea21ab42e240
Felipe Tomazelli <fefetl08@gmail.com>
2020-05-26 01:10:20 -0300

    alfabeto bin

--- trabalhoRA3/criptoBitXor.py
+++ trabalhoRA3/criptoBitXor.py
@@ -1,8 +1,8 @@
-alfabeto = {'A': 24, 'B': 19, 'C': 14, 'D': 18, 'E': 16, 'F': 22, 'G': 11,
-            'H': 5, 'I': 12, 'J': 26, 'K': 30, 'L': 9, 'M': 7, 'N': 6,
-            'O': 3, 'P': 13, 'Q': 29, 'R': 10, 'S': 20, 'T': 1, 'U': 28,
-            'V': 15, 'W': 25, 'X': 23, 'Y': 21, 'Z': 17, '9': 4, '8': 31,
-            '+': 27, '4': 8, '3': 2, '/': 0}
+alfabeto = {'A': 0b11000, 'B': 0b10011, 'C': 0b01110, 'D': 0b10010, 'E': 0b10000, 'F': 0b10110, 'G': 0b01011,
+            'H': 0b00101, 'I': 0b01100, 'J': 0b11010, 'K': 0b11110, 'L': 0b01001, 'M': 0b00111, 'N': 0b00110,
+            'O': 0b00011, 'P': 0b01101, 'Q': 0b11101, 'R': 0b01010, 'S': 0b10100, 'T': 0b00001, 'U': 0b11100,
+            'V': 0b01111, 'W': 0b11001, 'X': 0b10111, 'Y': 0b10101, 'Z': 0b10001, '9': 0b00100, '8': 0b11111,
+            '+': 0b11011, '4': 0b01000, '3': 0b00010, '/': 0b00000}
 pontuacao = {' ': '9', '?': '8', '.': '+', ',': '3', '!': '4', ';': '/'}
 texto = 'RESOLUCAO DE PROBLEMAS DE NATUREZA DISCRETA; TRABALHO POR FELIPE TOMAZELLI'
 criptografado = 'SJX9XFP8QCZXJML9POB/NKMYCCH88FLXVDMYDKPPCG+MJGL8PDR3QCC9SCYXXNCXJG//N+BCXN'
@@ -39,5 +39,4 @@ def rf02(text, key):
 
 print(rf01(texto, chave))
 print(rf02(rf01(texto, chave), chave))
-
 
